e57040f3e1001a6908c927c5ec848fb2698775a3
Hiram Méndez <hirammendez000@gmail.com>
2025-06-06 15:19:04 -0600

    space stone

---
+++ src/main/java/com/debuggeandoideas/models/SpaceStone.java
@@ -0,0 +1,13 @@
+package com.debuggeandoideas.models;
+
+public class SpaceStone extends Stone{
+
+    private static final String COLOR = "Blue";
+    private static final String NAME = "Space Stone";
+    private static final String LOCATION = "Tesserac";
+    private static final int ENERGY_LEVEL = 7;
+
+    public SpaceStone(String color, String name, String location, Integer energyLevel, Integer numberOfSides) {
+        super(COLOR, NAME, LOCATION, ENERGY_LEVEL);
+    }
+}
